56832f32075094ac525abf9b261399e24d7f42fe
张京涛 <zhangjingtaosleep@163.com>
2015-05-19 11:22:59 +0800

    将数据库配置放到properties中

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,9 +1,14 @@
+import org.springframework.core.io.ClassPathResource
+import org.springframework.core.io.support.PropertiesLoaderUtils
+
+def properties = PropertiesLoaderUtils.loadProperties(new ClassPathResource('datasource.properties'))
+
 dataSource {
     pooled = true
-//    driverClassName = "org.h2.Driver"
-    driverClassName = "com.mysql.jdbc.Driver"
-    username = "root"
-    password = "123qwe"
+    driverClassName = properties.getProperty("dataSource.driverClassName")
+    username = properties.getProperty("dataSource.username")
+    password = properties.getProperty("dataSource.password")
+    url = properties.getProperty("dataSource.url")
 }
 hibernate {
     cache.use_second_level_cache = true
@@ -15,31 +20,16 @@ environments {
     development {
         dataSource {
             dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
-            //url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
-            url = "jdbc:mysql://localhost:3306/grailstest?useUnicode=true&characterEncoding=utf-8"
         }
     }
     test {
         dataSource {
             dbCreate = "update"
-            url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
         }
     }
     production {
         dataSource {
             dbCreate = "update"
-            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
-            pooled = true
-            properties {
-                maxActive = -1
-                minEvictableIdleTimeMillis = 1800000
-                timeBetweenEvictionRunsMillis = 1800000
-                numTestsPerEvictionRun = 3
-                testOnBorrow = true
-                testWhileIdle = true
-                testOnReturn = true
-                validationQuery = "SELECT 1"
-            }
         }
     }
 }
