72b4e7c41ff8edf3d3426a7eff2c1630ae0dc2ea
张京涛 <zhangjingtaosleep@163.com>
2015-05-20 18:06:12 +0800

    修正chrome用户为任意字符，确保匿名

--- grails-app/domain/ChromeUser.groovy
+++ grails-app/domain/ChromeUser.groovy
@@ -4,12 +4,12 @@
 class ChromeUser {
 
     static constraints = {
-        email(unique: true,email: true)
+        email(unique: true)
     }
 
     String userToken//匿名数据 token
     String accessToken//访问数据 token
-    String email//注册Email
+    String email//注册Email --update 任意字符串即可
     Date dateCreted //创建时间
     String source//来源
 
