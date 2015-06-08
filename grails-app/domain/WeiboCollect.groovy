614ebce048d19ce4bf85304c24277e588cf7fcba
张京涛 <zhangjingtaosleep@163.com>
2015-06-08 23:13:54 +0800

    实现微博@功能，根据@发起用户分类

---
+++ grails-app/domain/WeiboCollect.groovy
@@ -0,0 +1,45 @@
+/**
+ * Weibo用户
+ */
+class WeiboCollect {
+
+    static constraints = {
+        reWeiboCreateDate(nullable: true)
+        reWeiboId(nullable: true)
+        reText(nullable: true)
+        reWeiboUserName(nullable: true)
+        reWeiboUserId(nullable: true)
+        comments(nullable: true)
+        commentId(nullable: true)
+    }
+
+    /**
+     * @的微博
+     */
+    String weiboCreateDate
+    String weiboId
+    String text
+    String weiboUserName
+    String weiboUserId
+    /**
+     * 如果是转发微博以下字段不为空
+     */
+    String reWeiboCreateDate
+    String reWeiboId
+    String reText
+    String reWeiboUserName
+    String reWeiboUserId
+    /**
+     * 评论相关
+     */
+    String comments//评论内容
+    String commentUserId
+    String commentUserName
+    String commentDate
+
+    Date createdDate
+    String commentId
+
+
+
+}
---
+++ grails-app/domain/WeiboJobHistory.groovy
@@ -0,0 +1,12 @@
+/**
+ * Weibo用户
+ */
+class WeiboJobHistory {
+
+    static constraints = {
+    }
+
+    String lastWeiboId
+    Date runDate
+    Boolean successed
+}
