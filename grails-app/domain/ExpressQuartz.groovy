444519ac61b770497ec5b85ad2a02589000056a7
张京涛 <zhangjingtaosleep@163.com>
2015-07-05 21:08:32 +0800

    定义快递信息查询数据库表=====(￣▽￣*)b

---
+++ grails-app/domain/ExpressQuartz.groovy
@@ -0,0 +1,20 @@
+/**
+ * 待查快递
+ */
+class ExpressQuartz {
+
+    static constraints = {
+
+    }
+
+
+    String expressNo
+    String company
+    Date lastUpdate
+    String lastQueryJson
+    String lastNu
+    Boolean unChecked
+    String notiEmail
+
+
+}
---
+++ grails-app/domain/ExpressQuartzEmailLog.groovy
@@ -0,0 +1,17 @@
+/**
+ * 快递邮件发送记录
+ */
+class ExpressQuartzEmailLog {
+
+    static constraints = {
+
+    }
+
+    
+    ExpressQuartz quartz
+    String title
+    String content
+    Date sendTime
+
+
+}
